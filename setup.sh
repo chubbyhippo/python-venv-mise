9bf9d8b0de38ae45433f087baf643061ac2ef23f
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-04-22 18:51:55 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,8 @@
-
+#!/usr/bin/env sh
+curl -k "https://raw.githubusercontent.com/chubbyhippo/python-venv-mise/refs/heads/main/mise.toml" -o "mise.toml"
+mise trust
+mise install
+curl -k "https://raw.githubusercontent.com/chubbyhippo/python-venv-mise/refs/heads/main/requirements.txt" -o "requirements.txt"
+mise exec python@3 -- python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
