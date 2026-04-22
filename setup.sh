c0d372eb5cd4c6e6c77475e5aedf87ea109098bd
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-04-22 21:11:26 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,25 @@ mise trust
 mise install
 curl -k "https://raw.githubusercontent.com/chubbyhippo/python-venv-mise-setup/refs/heads/main/requirements.txt" -o "requirements.txt"
 mise exec python@3 -- python -m venv .venv
-source .venv/bin/activate
+
+case $(uname 2>/dev/null) in
+Linux)
+	# For Linux systems
+	source .venv/bin/activate
+	;;
+Darwin)
+	# For macOS systems
+	source .venv/bin/activate
+	;;
+MINGW* | MSYS* | CYGWIN*)
+	# For Windows systems using MinGW/MSYS/Cygwin
+	source .venv/Scripts/activate
+	;;
+*)
+	# If the operating system is unknown
+	echo "Unknown Operating System" >&2
+	exit 1
+	;;
+esac
+
 pip install -r requirements.txt
